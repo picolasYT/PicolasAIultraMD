6e63f0c67eb2ffe9e8b379f32b4c350702029127
picolasYT <nicolasguevarakraan@gmail.com>
2025-09-04 14:07:52 -0300

    ACTUALIZACION IMPORTANTE config.js

--- config.js
+++ config.js
@@ -3,28 +3,28 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-   ['51921826291', 'SoyMaycol', true],
+   ['549299487598', 'SoyMaycol', true],
     ['180650938249287', 'SoyMaycol', true]
 ]
 
 global.mods = []
 global.prems = []
 
-global.namebot = '𝙼𝚊𝚢𝚌𝚘𝚕𝐏𝐥𝐮𝐬'
+global.namebot = 'PicolasAlultra-MD'
 global.redes = 'https://chat.whatsapp.com/KDI7NNovzdwJayx1gI1cue?mode=ems_copy_t'
-global.botname = '𝕄𝕒𝕪𝕔𝕠𝕝𝐏𝐥𝐮𝐬'
-global.banner = 'https://raw.githubusercontent.com/SoySapo6/tmp/refs/heads/main/Permanentes/images%20(8).jpeg'
-global.packname = '𝕄𝕒𝕪𝕔𝕠𝕝𝐏𝐥𝐮𝐬'
-global.author = '𝙃𝙚𝙘𝙝𝙤 𝙥𝙤𝙧 𝙎𝙤𝙮𝙈𝙖𝙮𝙘𝙤𝙡 <3'
-global.moneda = 'MayCoins'
+global.botname = 'PicolasAlultra-MD'
+global.banner = 'https://files.catbox.moe/k05foi.jpg'
+global.packname = 'PicolasAlultra-MD'
+global.author = 'Hecho por Picolas <3'
+global.moneda = 'PicoCoins'
 global.libreria = 'Baileys'
 global.baileys = 'V 6.7.16'
 global.vs = '2.2.0'
-global.sessions = 'MayBot'
-global.jadi = 'MayBots'
+global.sessions = 'PicolasSession'
+global.jadi = 'PicolasBots'
 global.yukiJadibts = true
 
-global.namecanal = '𝐒𝐨𝐲𝐌𝐚𝐲𝐜𝐨𝐥 <𝟑 • Actualizaciones'
+global.namecanal = 'PicolasAlultra • Actualizaciones'
 global.idcanal = '120363372883715167@newsletter'
 global.idcanal2 = '120363372883715167@newsletter'
 global.canal = 'https://whatsapp.com/channel/0029VayXJte65yD6LQGiRB0R'
