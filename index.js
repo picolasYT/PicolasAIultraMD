7ccc229c1c080b47995457f335fe4306d3d328a7
picolasYT <nicolasguevarakraan@gmail.com>
2025-09-05 15:15:24 -0300

    ☆ {ℙ𝕚𝕔𝕠𝕝𝕒𝕤𝔸𝕀𝐮𝐥𝐭𝐫𝐚-𝐌𝐃} ☆

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './config.js'
 import cluster from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
